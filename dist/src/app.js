0059174165676e4a4443e3ae202aeb5251aa397f
Makerjunior <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>
2024-03-16 18:09:44 -0300

    update

--- dist/src/app.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const _1 = require(".");
-_1.main.server.listen(3000, () => console.log(`Server is running 3000`));
--- dist/variaveis.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PATH = void 0;
-exports.PATH = __dirname;
