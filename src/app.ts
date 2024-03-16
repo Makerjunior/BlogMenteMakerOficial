d546ab9956966a17cc9c41b19a4844a8b36e9b93
Mekerjunior <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>
2024-03-16 00:24:21 -0300

    Flask

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,6 @@
-import express = require("express");
-import { router } from "./router";
-import path = require("path");
+import dotenv = require('dotenv');
+import { main } from ".";
+dotenv.config();
 
-class  Main{
-  private _server;
-  constructor(){
-    this._server = express();
-    this._middleware();
-    this._router();
-    this._server.use(express.static(path.join(__dirname, '../public')))
-    
-  }
-  private _middleware(){
-    this._server.use(express.json());
-  }
-  private  _router() {
-    this._server.use(router)
-  }
-
-  get server(){
-    return this._server;
-  }
-
-
-
-}
-
-export const main = new  Main()
+main.server.listen(process.env.PORT ,
+     () => console.log(`Server is running ${process.env.PORT}`));
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,29 @@
-import dotenv = require('dotenv');
-import { main } from "./app";
-dotenv.config();
+import express = require("express");
+import { router } from "./router";
+import path = require("path");
 
-main.server.listen(process.env.PORT ,
-     () => console.log(`Server is running ${process.env.PORT}`));
+class  Main{
+  private _server;
+  constructor(){
+    this._server = express();
+    this._middleware();
+    this._router();
+    this._server.use(express.static(path.join(__dirname, '../public')))
+    
+  }
+  private _middleware(){
+    this._server.use(express.json());
+  }
+  private  _router() {
+    this._server.use(router)
+  }
+
+  get server(){
+    return this._server;
+  }
+
+
+
+}
+
+export const main = new  Main()
